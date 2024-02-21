aae795408681f2954f658b18960bf01e5a39ca67
LapinGit <lapingithub@gmail.com>
2024-02-21 15:41:08 +0800

    create pages and routes

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-"# community-polling-" 
+"# community-polling" 
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import SignIn from "./Pages/SignIn";
+import SignUp from "./Pages/SignUp";
+import DashBoard from "./Pages/DashBoard";
+import Projects from "./Pages/Projects";
+
 export default function App() {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/dashboard" element={<DashBoard />} />
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
