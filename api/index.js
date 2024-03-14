11b3d4658e6953f01084017c8a9e2e683107d9dd
LapinGit <lapingithub@gmail.com>
2024-03-14 16:55:23 +0800

    Fixed Index read

--- api/index.js
+++ api/index.js
@@ -7,9 +7,19 @@ import postRoutes from "./routes/post.route.js";
 import voteRoutes from "./routes/vote.route.js";
 import commentRoutes from "./routes/comment.route.js";
 import path from "path";
+dotenv.config();
+
+mongoose
+.connect(process.env.MONGO)
+.then(() => {
+  console.log("Mongodb is connected");
+})
+.catch((err) => {
+  console.log(err);
+});
 
 const __dirname = path.resolve();
-app.use(express.static(path.join(__dirname, '/client/dist')));
+const app = express();
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
@@ -18,22 +28,12 @@ app.get('*', (req, res) => {
 
 import cookieParser from "cookie-parser";
 
-const app = express();
-dotenv.config();
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000!");
 });
 
-mongoose
-  .connect(process.env.MONGO)
-  .then(() => {
-    console.log("Mongodb is connected");
-  })
-  .catch((err) => {
-    console.log(err);
-  });
-
+app.use(express.static(path.join(__dirname, '/client/dist')));
 app.use(express.json());
 app.use(cookieParser());
 
